e78e2f8b56cdaa2d7eaeb7da6c4ce107fdf2e958
Zaigham Gulzar <56345914+Zaigham-Gulzar@users.noreply.github.com>
2024-06-14 17:57:45 +0500

    authentication completed

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
 import { Container,Nav,Navbar,Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AuthContext } from '../context/AuthContext';
+
 
 const NavBar = () => {
+  const {user,logoutUser} = useContext(AuthContext);
+  console.log("user",user);
     return (
     <>
       <Navbar bg="dark" className="mb-4" style={{height:"3.7rem"}}>
@@ -9,15 +14,23 @@ const NavBar = () => {
             <h2>
                 <Link to="/chat" className="link-light text-decoration-none">ChatApp</Link>
             </h2>
-            <span className="text-warning">Logged in as Charles</span>
+           {user && <>
+            <span className="text-warning">Logged in as {user?.name}</span>
+           </>}
             <Nav>
                 <Stack direction="horizontal" gap='3'>
-                  <Link to='/login' className="link-light text-decoration-none">
+                  {user && <>
+                    <Link onClick={(e)=>logoutUser()} to='/' className="link-light text-decoration-none">
+                    Logout
+                  </Link>
+                  </>}
+                  {!user && <>
+                    <Link to='/login' className="link-light text-decoration-none">
                     Login
                   </Link>
                   <Link to='/register' className="link-light text-decoration-none">
                     Register
-                  </Link>
+                  </Link></>}
                 </Stack>
             </Nav>
         </Container>
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,18 +1,85 @@
-import React, { Children, createContext,useState } from "react";
+import React, { Children, createContext,useCallback,useEffect,useState } from "react";
+import { baseUrl, postRequest } from "../utils/services";
 
 export const AuthContext = React.createContext();
 
-
 export const AuthContextProvider = ({children})=>{
-
-    const [user,setUser] = useState({
-        name:"Charles"
+    const [user,setUser] = useState(null);
+    const [registerError,setRegisterError] = useState(null);
+    const [isRegisterLoading,setIsRegisterLoading] = useState(false);
+    const [loginError,setLoginError] = useState(null);
+    const [isLoginLoading,setIsLoginLoading] = useState(false);
+    const [registerInfo,setRegisterInfo] = useState({
+        name:"",
+        email:"",
+        password:""
     });
+    const [loginInfo,setLoginInfo] = useState({
+        email:"",
+        password:""
+    });
+    
+    console.log(loginInfo);
+
+    console.log(user);
+
+    useEffect(()=>{
+      const getUser = localStorage.getItem("user");
+      setUser(JSON.parse(getUser));
+    },[]);
+
+    const updateRegisterInfo = useCallback((info)=>{
+       setRegisterInfo(info);
+    },[]);
+
+    const updateLoginInfo = useCallback((info)=>{
+        setLoginInfo(info);
+     },[]);
+ 
+    const registerUser = useCallback(async(e)=>{
+        e.preventDefault();
+
+        setIsRegisterLoading(true);
+        setRegisterError(null);
+        const response =  await postRequest(`${baseUrl}/users/register`,JSON.stringify(registerInfo));
+
+        if(response.error){ 
+            return setRegisterError(response);
+        }
+        setIsRegisterLoading(false);
+
+        localStorage.setItem("user",JSON.stringify(response));
+        setUser(response);
+    },[registerInfo]);
+
+
+    const logoutUser = useCallback(async(e)=>{
+        localStorage.removeItem("user");
+        setUser(null); 
+    },[]);
+
+    const loginUser = useCallback(async (e)=>{
+        e.preventDefault();
+        setIsLoginLoading(true);
+        setLoginError(null);
+        console.log(loginInfo);
+        const response =  await postRequest(`${baseUrl}/users/login`,JSON.stringify(loginInfo));
+        console.log(response);
+        
+        if(response.error){ 
+            return setLoginError(response);
+        }
+        setIsLoginLoading(false);
+
+        localStorage.setItem("user",JSON.stringify(response))
+        setUser(response);
+    },[loginInfo]);
+
+
 
     return (
-        <AuthContext.Provider value={{user}}>
+        <AuthContext.Provider value={{user,registerInfo,updateRegisterInfo,registerUser,registerError,isRegisterLoading,logoutUser,updateLoginInfo,loginError,isLoginLoading,loginInfo,loginUser}}>
             {children}
         </AuthContext.Provider>
     )
-
 }
